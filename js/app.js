66b3cc10f2550c35f27eab599013c0d4e1a90122
Johán Suárez <32972612+JohanSuarezQ@users.noreply.github.com>
2020-08-15 11:49:22 -0500

    Cart page finished

---
+++ js/app.js
@@ -0,0 +1,122 @@
+
+const shop = document.getElementById('store');
+const pokeCache = {}
+
+const fetchproducto = async () => {
+    const url = './js/API.json'
+    const res = await fetch(url)
+    const producto = await res.json()
+    const prodAlmacenado = JSON.stringify(producto)
+    localStorage.setItem('prod', prodAlmacenado)
+    displayproducto(producto)
+}
+
+const displayproducto = (producto) => {
+    const productoHTMLString = producto
+        .map((prod) => `
+        <li class="card" >
+            <h2 class="card-title">${prod.name}</h2>
+            <img alt = "image-of-${prod.name}" class="card-image" src="${prod.image}"/>
+            <p class="card-subtitle" onclick = "selectproducto(${prod.id})">CLICK ME!</p>
+        </li>
+    `
+        )
+        .join('');
+        const icon = document.getElementById('cart-icon')
+        const sumatoria = document.getElementById('sumatoria-productos')
+        const conteo = document.getElementById('conteo-productos')
+        const contator = localStorage.getItem('conteo')
+        const total = JSON.parse(localStorage.getItem('total'))
+        
+        console.log(typeof(total));
+        if(total>0){
+            icon.innerHTML = `<a><i class="material-icons md-36" style="color: orange">add_shopping_cart</i></a>`
+            conteo.innerHTML = `<p>${contator}</p>`
+            sumatoria.innerHTML = `<p>$ ${total}</p>`
+        }
+    shop.innerHTML = productoHTMLString;
+};
+
+const selectproducto = async (id)=>{
+        const url = `./js/API.json`
+        const res = await fetch (url)
+        const prod = await res.json()
+        const productId= prod[id-1]
+        displayPopup(productId)
+}
+
+const displayPopup = (productId) =>{
+    const image = productId.image
+    const htmlString = `
+    <div class = "popup" id = "popup">
+    <button id = "closeBtn" onclick = "closePopup()">Close</button>
+    <div class="card-popup">
+            <img class="card-image-popup"  src="${image}" />
+            <h2 class="card-title-popup">${productId.name}</h2>
+            <p>${productId.description}</p>
+            <p class="card-subtitle-modal"><b>Price: $ ${productId.price}</b></p>
+            <button id="botoncito" onclick = "displayId(${productId.id})">I WANT IT!!</button>
+        </div>
+    </div>
+    `
+    shop.innerHTML = htmlString + shop.innerHTML
+}
+
+const closePopup = () =>{
+    const popup = document.querySelector('.popup')
+    popup.parentElement.removeChild(popup)
+
+}
+
+const findOrCreateCartProduct = () => {
+    if (!localStorage.getItem('cart')) {
+      localStorage.setItem('cart', JSON.stringify([]));
+    }
+    return JSON.parse(localStorage.getItem('cart'));
+  }
+
+  const createCart = (productId, array) =>{
+    let productArr = findOrCreateCartProduct();
+            
+    let itExist = productArr.find(item => item.id == productId);
+    if (!itExist) {
+      productArr.push({ ...array[productId - 1], count: 1 });
+    } else {
+      itExist.count += 1;
+    }
+    localStorage.setItem('cart', JSON.stringify(productArr));
+  }
+
+const displayId = (productId) =>{
+    const array = JSON.parse(localStorage.getItem('prod'))
+    const precio = array[productId-1].price
+    const nombre = array[productId-1].name
+    var conteoProductos = localStorage.getItem('conteo')
+    var total = localStorage.getItem('total')
+    const icon = document.getElementById('cart-icon')
+    const sumatoria = document.getElementById('sumatoria-productos')
+    const conteo = document.getElementById('conteo-productos')
+    console.log((total))
+    createCart(productId, array)
+    if(total==null && conteoProductos==null){
+        conteoProductos = 1
+        localStorage.setItem('total', precio)
+        localStorage.setItem('conteo', conteoProductos)
+        conteo.innerHTML = `<p>${conteoProductos}</p>`
+        sumatoria.innerHTML = `<p>$ ${precio}</p>`
+        icon.innerHTML = `<a><i class="material-icons md-36" style="color: orange">add_shopping_cart</i></a>`
+        alert(`Thanks! ${nombre} ADDED, your new total is $ ${precio}`)
+    }else{
+        total = (parseFloat(total) + precio).toFixed(2)
+        conteoProductos = parseInt(conteoProductos)+1   
+        localStorage.setItem('total', total)
+        localStorage.setItem('conteo', conteoProductos)
+        sumatoria.innerHTML = `<p>$ ${total}</p>`
+        conteo.innerHTML = `<p>${conteoProductos}</p>`
+        icon.innerHTML = `<a><i class="material-icons md-36" style="color: orange">add_shopping_cart</i></a>`
+        alert(`Thanks! ${nombre} ADDED, your new total is $ ${total}`)
+    }
+}
+
+
+fetchproducto();
---
+++ js/cart.js
@@ -0,0 +1,154 @@
+let cartProducts = JSON.parse(localStorage.getItem('cart'));
+console.log(cartProducts)
+const details = document.getElementById('details')
+const printProducts = document.getElementById('cart')
+const totalProducts = document.getElementById('total')
+
+function createCart (){
+    if(cartProducts.length !== 0){
+        for(let i = 0; i< cartProducts.length; i++){
+            const cartSide = cartProducts[i]
+            console.log(cartSide.price);
+            printProducts.innerHTML += `
+            <li id = "list-cart${i}" class = "list-cart">
+                <h2 class = "list-cart-title"><small>${cartSide.name}</small></h2>
+                <img class = "list-cart-image" width = "100" src="${cartSide.image}" alt="">
+                <p class = "list-cart-description"><small>${cartSide.description}</small></p>
+                <p class = "list-cart-price"></p>
+                <p id = "price${i}" class = "list-cart-amount"><small>Price: $ ${cartSide.price}</small></p>
+                <button class="more" onclick = "countMore(${cartSide.id}, ${cartSide.count})"><small>ADD</small></button>
+                <button class="menos" onclick = "countMenos(${cartSide.id}, ${cartSide.count})"><small>REMOVE</small></button>
+            </li>`
+            const total = localStorage.getItem('total')
+            const conteo = localStorage.getItem('conteo')
+            details.innerHTML = `
+            <h3 class = "details-title">DETAILS<h3>
+            <h3 class = "details-total">Total:</h3>
+                <p class = "details-total-number">$ ${total}</p> 
+            <h3 class = "details-amount">Products Amount:</h3>
+                <p class = "details-amount-number">${conteo}</p>
+                <button class="delete-car" onclick = "deleteCar()">DELETE CAR</button>
+            `
+            const acumulado = document.getElementsByTagName('li')[i].querySelector('.list-cart-price')
+            console.log(acumulado);
+            acumulado.innerHTML = `Amount: ${cartProducts[i].count}`
+            }
+    }
+}
+function countMore (id, count){
+    for(let i = 0; i< cartProducts.length; i++){
+        if(cartProducts[i].id===id){
+            var conteo = parseInt(localStorage.getItem('conteo'))
+            var total = parseFloat(localStorage.getItem('total'))
+            total = (total + cartProducts[i].price).toFixed(2)
+            cartProducts[i].count += 1
+            conteo++
+            localStorage.setItem('conteo', conteo)
+            localStorage.setItem('total', total)
+            localStorage.setItem('cart', JSON.stringify(cartProducts))
+            console.log(`el contador de ${cartProducts[i].name}  ${cartProducts[i].count}`)
+            details.innerHTML = `
+            <h3 class = "details-title">DETAILS<h3>
+            <h3 class = "details-total">Total:</h3>
+                <p class = "details-total-number">$ ${total}</p> 
+            <h3 class = "details-amount">Products Amount:</h3>
+                <p class = "details-amount-number">${conteo}</p>
+                <button class="delete-car" onclick = "deleteCar()">DELETE CAR</button>
+            `
+            const acumulado = document.getElementsByTagName('li')[i].querySelector('.list-cart-price')
+            console.log(acumulado);
+            acumulado.innerHTML = `Amount: ${cartProducts[i].count}`
+            
+        }
+    }
+}
+
+function countMenos (id, count){
+    for(let i = 0; i< cartProducts.length; i++){
+        if(cartProducts[i].id===id){
+            var conteo = parseInt(localStorage.getItem('conteo'))
+            var total = parseFloat(localStorage.getItem('total'))
+            total = (total - cartProducts[i].price).toFixed(2)
+            cartProducts[i].count -= 1
+            conteo--
+            localStorage.setItem('conteo', conteo)
+            localStorage.setItem('total', total)
+            localStorage.setItem('cart', JSON.stringify(cartProducts))
+            details.innerHTML = `
+            <h3 class = "details-title">DETAILS<h3>
+            <h3 class = "details-total">Total:</h3>
+                <p class = "details-total-number">$ ${total}</p> 
+            <h3 class = "details-amount">Products Amount:</h3>
+                <p class = "details-amount-number">${conteo}</p>
+                <button class="delete-car" onclick = "deleteCar()">DELETE CAR</button>
+            `
+            const acumulado = document.getElementsByTagName('li')[i].querySelector('.list-cart-price')
+            console.log(acumulado);
+            acumulado.innerHTML = `Amount: ${cartProducts[i].count}`
+
+            console.log(`el contador de ${cartProducts[i].name}  ${cartProducts[i].count}`)
+            if(cartProducts[i].count<1){
+                cartProducts.splice(i, 1)
+                console.log(cartProducts);
+                localStorage.setItem('cart', JSON.stringify(cartProducts))
+                const padre = document.getElementById('cart')
+                const hijo = document.getElementsByTagName("li")[i]
+                const removido = padre.removeChild(hijo)
+                if(cartProducts.length===0){
+                    localStorage.removeItem('cart')
+                    console.log('el arreglo esta vacio')
+                    printProducts.innerHTML = `
+                    <i class="material-icons md-48" id="sad-icon">sentiment_very_dissatisfied</i>
+                    <h3>Your cart is empty click <button><a class="back-button" href="index.html">HERE!</a></button></h3>
+                    `
+                }
+            }
+        }
+    }
+}
+
+function deleteCar(){
+    console.log('este es el boton de eliminar ');
+    const cartProducts = ''
+    const total = 0
+    const conteo = 0
+    console.log(printProducts);
+    localStorage.setItem('cart', cartProducts)
+    localStorage.setItem('conteo', conteo)
+    localStorage.setItem('total', total)
+    printProducts.innerHTML = `
+    <i class="material-icons md-48" id="sad-icon">sentiment_very_dissatisfied</i>
+    <h3>Your cart is empty click <button><a class="back-button" href="index.html">HERE!</a></button></h3>
+    `
+    details.innerHTML = `
+            <h3 class = "details-title">DETAILS<h3>
+            <h3 class = "details-total">Total:</h3>
+                <p class = "details-total-number">$ 0.0</p> 
+            <h3 class = "details-amount">Products Amount:</h3>
+                <p class = "details-amount-number">0</p>
+                <button class="delete-car" onclick = "deleteCar()">DELETE CAR</button>
+            `
+    localStorage.removeItem('cart')
+}
+
+function cartPageEmpty(){
+    if(cartProducts===null){
+        printProducts.innerHTML = `
+        <i class="material-icons md-48" id="sad-icon">sentiment_very_dissatisfied</i>
+        <h3>Your cart is empty click <button><a class="back-button" href="index.html">HERE!</a></button></h3>
+        `
+        details.innerHTML = `
+            <h3 class = "details-title">DETAILS<h3>
+            <h3 class = "details-total">Total:</h3>
+                <p class = "details-total-number">$ 0.0</p> 
+            <h3 class = "details-amount">Products Amount:</h3>
+                <p class = "details-amount-number">0</p>
+                <button class="delete-car" onclick = "deleteCar()">DELETE CAR</button>
+            `
+    }
+}
+
+cartPageEmpty()
+
+createCart()
+
